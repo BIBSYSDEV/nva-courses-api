0bd704288e9d3547d7edf0e916862a20cd2ae4d5
Ketil Aasarød <ketil.aasarod@sikt.no>
2022-07-19 09:53:52 +0200

    added annotations again, as well as @JsonCreator annotation on constructor

--- courses-rest/src/main/java/no/sikt/nva/fs/Course.java
+++ courses-rest/src/main/java/no/sikt/nva/fs/Course.java
@@ -1,5 +1,7 @@
 package no.sikt.nva.fs;
 
+import com.fasterxml.jackson.annotation.JsonCreator;
+import com.fasterxml.jackson.annotation.JsonProperty;
 import java.util.Objects;
 import nva.commons.core.JacocoGenerated;
 
@@ -9,7 +11,10 @@ public final class Course {
     private final String term;
     private final int year;
 
-    public Course(final String code, final String term, int year) {
+    @JsonCreator
+    public Course(@JsonProperty("code") final String code,
+                  @JsonProperty("term") final String term,
+                  @JsonProperty("year") int year) {
         this.code = code;
         this.term = term;
         this.year = year;
